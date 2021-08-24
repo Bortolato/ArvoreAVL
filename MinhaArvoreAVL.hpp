9c6b74cc1ae1307ab2023c2c06eaa728d5139053
Bruno Bortolato <bbortolato31@gmail.com>
2021-08-24 00:06:31 -0300

    algumas atualizações

--- MinhaArvoreAVL.hpp
+++ MinhaArvoreAVL.hpp
@@ -11,6 +11,8 @@
 template <typename T>
 class MinhaArvoreAVL final : public MinhaArvoreDeBuscaBinaria<T>
 {
+    // implementar as funções inserção, remoção e as 4 rotações
+
     //implemente aqui
 };
 
--- MinhaArvoreDeBuscaBinaria.hpp
+++ MinhaArvoreDeBuscaBinaria.hpp
@@ -11,14 +11,24 @@
 template <typename T>
 class MinhaArvoreDeBuscaBinaria : public ArvoreDeBuscaBinaria<T>
 {
-    ArvoreDeBuscaBinaria();
-    virtual ~ArvoreDeBuscaBinaria();
+private:
+    int quantidadeChaves;
+
+public:
+    ArvoreDeBuscaBinaria()
+    {
+        this->_raiz = nullptr;
+        this->quantidadeChaves = 0;
+    }
+
+    // percorrer toda a árvore para eliminar nodo por nodo (usar delete?)
+    virtual ~ArvoreDeBuscaBinaria() {}
 
     /**
      * @brief Verifica se a arvore esta vazia
      * @return Verdade se a arvore esta vazia.
      */
-    virtual bool vazia()
+    virtual bool vazia() const
     {
         if (this->_raiz == nullptr)
         {
@@ -34,7 +44,7 @@ class MinhaArvoreDeBuscaBinaria : public ArvoreDeBuscaBinaria<T>
      * @brief Retornar quantidade de chaves na arvore
      * @return Numero natural que representa a quantidade de chaves na arvore
      */
-    virtual int quantidade()
+    virtual int quantidade() const
     {
         if (vazia())
         {
@@ -42,6 +52,7 @@ class MinhaArvoreDeBuscaBinaria : public ArvoreDeBuscaBinaria<T>
         }
         else
         {
+            return this->quantidadeChaves;
         }
     }
 
@@ -50,17 +61,16 @@ class MinhaArvoreDeBuscaBinaria : public ArvoreDeBuscaBinaria<T>
      * @param chave chave a ser procurada na arvore
      * @return Verdade se a arvore contem a chave
      */
-    virtual bool contem(T chave)
+    virtual bool contem(T chave) const
     {
 
-        Nodo *aux = _raiz;
+        Nodo<T> *aux = this->_raiz;
 
-        while (aux != 0)
+        while (aux != nullptr)
         {
-
             if (chave == aux->chave)
             {
-                return true
+                return true;
             }
 
             // se a chave for menor
@@ -83,15 +93,12 @@ class MinhaArvoreDeBuscaBinaria : public ArvoreDeBuscaBinaria<T>
      * @param chave chave que é raiz da (sub)arvore cuja altura queremos. Se chave é nula, retorna a altura da arvore.
      * @return Numero inteiro representando a altura da (subarvore). Se chave nao esta na arvore, retorna std::nullopt
      */
-    virtual std::optional<int> altura(T chave)
+    virtual std::optional<int> altura(T chave) const
     {
-        if (chave == nullptr)
-        {
-            return _raiz->altura;
-        }
-        Nodo *aux = _raiz;
 
-        while (aux != 0)
+        Nodo<T> *aux = this->_raiz;
+
+        while (aux != nullptr)
         {
 
             if (chave == aux->chave)
@@ -111,7 +118,7 @@ class MinhaArvoreDeBuscaBinaria : public ArvoreDeBuscaBinaria<T>
                 aux = aux->filhoDireita;
             }
         }
-        return std::nullopt
+        return std::nullopt;
     }
 
     /**
@@ -124,15 +131,17 @@ class MinhaArvoreDeBuscaBinaria : public ArvoreDeBuscaBinaria<T>
         // insere na raiz
         if (_raiz == nullptr)
         {
-            Nodo<T> *novo = (Nodo<T> *)calloc(1, sizeof(Nodo<T>));
+            Nodo<T> *novo = new Nodo<T>; //Esta certo ??
             novo->chave = chave;
             novo->filhoEsquerda = nullptr;
             novo->filhoDireita = nullptr;
             novo->altura = 0;
             return novo;
         }
+        // insere no restante da arvore
         else
         {
+            if ()
         }
     }
 
@@ -150,42 +159,85 @@ class MinhaArvoreDeBuscaBinaria : public ArvoreDeBuscaBinaria<T>
      * @param chave chave da arvore que eh pai do filho a esquerda
      * @return Chave do filho a esquerda. Se chave nao esta na arvore, retorna std::nullopt
      */
-    virtual std::optional<T> filhoEsquerdaDe(T chave) const = 0;
+    virtual std::optional<T> filhoEsquerdaDe(T chave) const
+    {
+    }
 
     /**
      * @brief Busca a chave do filho a direita de uma (sub)arvore
      * @param chave chave da arvore que eh pai do filho a direita
      * @return Chave do filho a direita. Se chave nao esta na arvore, retorna nullptr
      */
-    virtual std::optional<T> filhoDireitaDe(T chave) const = 0;
+    virtual std::optional<T> filhoDireitaDe(T chave) const
+    {
+    }
 
     /**
      * @brief Lista chaves visitando a arvore em ordem
      * @return Lista encadeada contendo as chaves em ordem. 
      */
-    virtual ListaEncadeadaAbstrata<T> *emOrdem() const = 0;
+    virtual ListaEncadeadaAbstrata<T> *emOrdem() const
+    {
+        ListaEncadeadaAbstrata<T> *lista = new MinhaListaEncadeada<T>;
+        Nodo *aux = this->_raiz;
+
+        if (this->_raiz != nullptr)
+        {
+            emOrder_aux(aux->filhoEsquerda, lista);
+            adicionaNoFim(this->_raiz->chave);
+            emOrder_aux(aux->filhoDireita, lista);
+        }
+    }
 
     /**
      * @brief Lista chaves visitando a arvore em pre-ordem
      * @return Lista encadeada contendo as chaves em pre-ordem. 
      */
-    virtual ListaEncadeadaAbstrata<T> *preOrdem() const = 0;
+    virtual ListaEncadeadaAbstrata<T> *preOrdem() const
+    {
+        ListaEncadeadaAbstrata<T> *lista = new MinhaListaEncadeada<T>;
+        Nodo<T> *aux = this->_raiz;
+
+        if (this->_raiz != nullptr)
+        {
+            // esta mesmo chamando a função??
+            lista->adicionaNoFim(aux->chave);
+            // como declarar nova função??
+            preOrdem_aux(aux->filhoEsquerda, lista);
+            preOrdem_aux(aux->filhoDireita, lista);
+        }
+    }
+
+    void preOrdem_aux(Nodo<T> *aux, ListaEncadeadaAbstrata<T> *l)
+    {
+        if (aux->filhoEsquerda == nullptr)
+        {
+            l->adicionaNoFim(aux->chave)
+        }
+        else
+        {
+            preOrdem_aux(aux->filhoEsquerda, l);
+        }
+
+        return
+    }
 
     /**
      * @brief Lista chaves visitando a arvore em pos-ordem
      * @return Lista encadeada contendo as chaves em pos ordem. 
      */
-    virtual ListaEncadeadaAbstrata<T> *posOrdem() const = 0;
+    virtual ListaEncadeadaAbstrata<T> *posOrdem() const
+    {
+        ListaEncadeadaAbstrata<T> *lista = new MinhaListaEncadeada<T>;
+        Nodo *aux = this->_raiz;
+
+        if (this->_raiz != nullptr)
+        {
+            posOrdem_aux(aux->filhoEsquerda, lista);
+            posOrdem_aux(aux->filhoDireita, lista);
+            adicionaNoFim(this->_raiz->chave);
+        }
+    }
 };
-
-template <typename T>
-ArvoreDeBuscaBinaria<T>::ArvoreDeBuscaBinaria() : _raiz{}
-{
-}
-
-template <typename T>
-ArvoreDeBuscaBinaria<T>::~ArvoreDeBuscaBinaria() = default;
-}
-;
 
 #endif
